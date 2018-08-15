f767ce29a682acbc0809d89551127502cb09864d
andrebrowne <42154231+andrebrowne@users.noreply.github.com>
2018-08-15 09:35:48 -0400

    Add on this day controller (#1)
    * Add logic for sorting and organizing on this day entries into categories
    
    * Rename (unused) builder file
    
    * Remove unused routes

--- app/controllers/otds_controller.rb
+++ app/controllers/otds_controller.rb
@@ -1,14 +1,40 @@
 require 'json'
 class OtdsController < ApplicationController
-  before_action :set_otd, only: [:show, :edit, :update, :destroy]
+  before_action :set_otd, only: :show
 
   # GET /otds
   # GET /otds.json
   def index
     @wiki_fact = Wikipedia.find(DateTime.now.strftime("%B %e"))
     @wiki_raw = @wiki_fact.text
-    @wiki_events = @wiki_raw.split("\n") #creates an array
-    @facts_of_the_day = @wiki_events.slice!(20,5)
-    @wiki_events = @facts_of_the_day
+    @wiki_events = @wiki_raw.split("\n")
+    @wiki_events_filtered = []
+
+    on_this_day = {
+      "== Events ==" => [],
+      "== Births ==" => [],
+      "== Deaths ==" => [],
+      "== Holidays and observances ==" => [],
+      "== References ==" => [],
+      "== External links ==" => []
+    }
+
+    current_section = nil
+    @wiki_events.each do |event|
+      if event.strip.length.zero?
+        next
+      end
+      if event.strip.start_with?('==')
+        current_section = event.strip
+        next
+      end
+      if current_section.nil?
+        next
+      end
+      on_this_day[current_section].push(event)
+    end
+    @facts_of_the_day_events = on_this_day["== Events =="].sample(5).sort_by { |x| x[/\d+/].to_i }
+    @facts_of_the_day_births = on_this_day["== Births =="].sample(5).sort_by { |x| x[/\d+/].to_i }
+    @facts_of_the_day_holidays = on_this_day["== Holidays and observances =="].sample(5).sort
   end
 end
---
+++ app/views/otds/_otd.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! otd, :id, :created_at, :updated_at
+json.url otd_url(otd, format: :json)
